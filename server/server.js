765fc4651e197690fb2309607ebd0d9d5761582a
SnehitaSN <129748111+SnehitaSN@users.noreply.github.com>
2025-09-28 17:19:58 +0530

    updated baseurl in server.js

--- server/server.js
+++ server/server.js
@@ -110,7 +110,9 @@ const JWT_SECRET = process.env.JWT_SECRET || "supersecretjwtkey"; // Use a stron
 
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000"; // ⭐ Ensure this line exists and is correct vercel url
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:5000";
+ //const BASE_URL = process.env.BASE_URL || "http://localhost:5000";
+
+const BASE_URL = process.env.BASE_URL || "https://tgsc.onrender.com";
 
 // // Middleware to protect routes and get user ID
 // const authenticateToken = (req, res, next) => {
